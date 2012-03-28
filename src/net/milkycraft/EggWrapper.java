ee8895619ee010be1940bc8a8eff92e6b02395a9
Nick Porillo <milkywayz@mail.com>
2012-03-28 17:12:56 -0400

    Minor edits by mit, and for some reason need to be synced again)

--- src/net/milkycraft/EggWrapper.java
+++ src/net/milkycraft/EggWrapper.java
@@ -5,23 +5,19 @@ import net.milkycraft.ASEConfiguration.EggConfiguration;
 public class EggWrapper extends org.bukkit.plugin.java.JavaPlugin {
     private EggConfiguration config;
     
-    @Override
-	public EggConfiguration getConfig() {
+    public EggConfiguration getConfig() {
         if(config == null) {
             config = new EggConfiguration(this);
         }
         return config;
     }
-    @Override
-	public void reloadConfig() {
+    public void reloadConfig() {
         getConfig().load();
     }
-    @Override
-	public void saveConfig() {
+    public void saveConfig() {
         getConfig().save();
     }
-    @Override
-	public void saveDefaultConfig() {
+    public void saveDefaultConfig() {
         getConfig().saveDefaults();
     }
 }
