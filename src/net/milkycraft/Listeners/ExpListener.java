837d0f11bf3c65ac3df10e1e221be91f149db16a
Nick Porillo <milkywayz@mail.com>
2012-03-28 20:19:28 -0400

    Removed check for xp bottles, and added eventhandler

--- src/net/milkycraft/Listeners/ExpListener.java
+++ src/net/milkycraft/Listeners/ExpListener.java
@@ -2,24 +2,23 @@ package net.milkycraft.Listeners;
 
 import net.milkycraft.Spawnegg;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.ExpBottleEvent;
 
 public class ExpListener implements Listener {
+	/*
+	 * Class tested and working
+	 */
 	Spawnegg plugin;
 	public ExpListener(Spawnegg instance) {
 		plugin = instance;
 	}
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onXpDrop(EntityDeathEvent e) {
 		if(plugin.getConfig().getBoolean("Disable.Experience")) {
 			e.setDroppedExp(0);
-			return;
-		}
-	}
-	public void XpBottle(ExpBottleEvent ev) {
-		if(plugin.getConfig().getBoolean("Disable.Experience")) {
-			ev.getEntity().remove();
 			return;
 		}
 	}
