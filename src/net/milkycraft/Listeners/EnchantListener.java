d6d381334222463e5c0a6cf7739297ac922984ac
Nick Porillo <milkywayz@mail.com>
2012-03-28 20:19:12 -0400

    Added return and event handler

--- src/net/milkycraft/Listeners/EnchantListener.java
+++ src/net/milkycraft/Listeners/EnchantListener.java
@@ -2,18 +2,25 @@ package net.milkycraft.Listeners;
 
 import net.milkycraft.Spawnegg;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 
 public class EnchantListener implements Listener {
+	/*
+	 * Class Tested and working
+	 */
 	Spawnegg plugin;
 	public EnchantListener(Spawnegg instance) {
 		plugin = instance;
 	}
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onEnchantAttempt(PrepareItemEnchantEvent e) {
 		if(plugin.getConfig().getBoolean("Disable.Enchanting") &&
 				!e.getEnchanter().hasPermission("entitymanager.enchanting")) {
 				e.setCancelled(true);
+				return;
 		}
 	}
 }
