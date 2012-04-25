7dfa1af155c329c78329bfba2eb3f7800f2f4e0b
Nick Porillo <milkywayz@mail.com>
2012-04-25 17:04:21 -0400

    Logging and better alerting, error messaging

--- src/net/milkycraft/Listeners/ThrowListener.java
+++ src/net/milkycraft/Listeners/ThrowListener.java
@@ -1,10 +1,11 @@
 package net.milkycraft.Listeners;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import net.milkycraft.Spawnegg;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.EnderCrystal;
@@ -20,138 +21,212 @@ public class ThrowListener implements Listener {
 	public ThrowListener(Spawnegg instance) {
 		plugin = instance;
 	}
+	public final Logger log = Logger.getLogger("Minecraft");
 
-	@EventHandler(priority = EventPriority.HIGH)
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void OnThrow(PlayerInteractEvent e) {
 		final Player player = e.getPlayer();
-		ChatColor green = ChatColor.GREEN;
-		ChatColor red = ChatColor.RED;
 		if (e.getItem() == null) {
 			return;
-		}							
+		}
 		List<String> worldz = plugin.getConfig().getStringList(
 				"World.Worldname");
 		for (String worldname : worldz) {
 			if (e.getPlayer().getWorld().getName().equals(worldname)) {
-				if (e.getAction() == Action.RIGHT_CLICK_BLOCK || 
-						e.getAction() == Action.RIGHT_CLICK_AIR) {									
-					 if (e.getItem().getTypeId() == 384) {
-							if (plugin.getConfig().getBoolean(
-									"block.Throw.XpBottles")
-									&& !player
-											.hasPermission("entitymanager.xpbottles")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to throw xp bottles");
-								return;
-							}
+				if (e.getAction() == Action.RIGHT_CLICK_BLOCK
+						|| e.getAction() == Action.RIGHT_CLICK_AIR) {
+					if (e.getItem().getTypeId() == 384) {
+						if (plugin.getConfig().getBoolean(
+								"block.Throw.XpBottles")
+								&& !player
+										.hasPermission("entitymanager.xpbottles")) {
+							e.setCancelled(true);
+							alerter(e);
+							messager(e);
 						}
-					else if (e.getItem().getTypeId() == 385) {
-							if (plugin.getConfig().getBoolean(
-									"block.Throw.FireCharges")
-									&& !player
-											.hasPermission("entitymanager.firecharges")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to throw fire charges");
-								return;
-							}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
 						}
-					else if (e.getItem().getTypeId() == 344) {
-							if (plugin.getConfig().getBoolean(
-									"block.Throw.ChickenEggs")
-									&& !player
-											.hasPermission("entitymanager.chickeneggs")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to throw eggs");
-								return;
-							}
+					} else if (e.getItem().getTypeId() == 385) {
+						if (plugin.getConfig().getBoolean(
+								"block.Throw.FireCharges")
+								&& !player
+										.hasPermission("entitymanager.firecharges")) {
+							e.setCancelled(true);
+							alerter(e);
+							messager(e);
 						}
-					else if (e.getItem().getTypeId() == 368) {
-							if (plugin.getConfig().getBoolean(
-									"block.Throw.EnderPearls")
-									&& !player
-											.hasPermission("entitymanager.enderpearl")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to use Ender Pearls");
-								return;
-							}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
 						}
-					else if (e.getItem().getTypeId() == 381) {
-							if (plugin.getConfig().getBoolean(
-									"block.Throw.EnderEyes")
-									&& !player
-											.hasPermission("entitymanager.endereye")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to use Ender Eyes");
-								return;
-							}
+					} else if (e.getItem().getTypeId() == 344) {
+						if (plugin.getConfig().getBoolean(
+								"block.Throw.ChickenEggs")
+								&& !player
+										.hasPermission("entitymanager.chickeneggs")) {
+							e.setCancelled(true);
+							messager(e);
 						}
-					else if (e.getItem().getTypeId() == 333) {
-							if (plugin.getConfig().getBoolean(
-									"block.Entities.Boats")
-									&& !player
-											.hasPermission("entitymanager.boat")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to use Boats");
-								return;
-							}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
 						}
-					else if (e.getItem().getTypeId() == 328) {
-							if (plugin.getConfig().getBoolean(
-									"block.Entities.Minecarts")
-									&& !player
-											.hasPermission("entitymanager.minecart")) {
-								e.setCancelled(true);
-								alert(player, e);
-								player.sendMessage(green
-										+ "[EM]"
-										+ red
-										+ " You dont have permission to use Minecarts");
-								return;
-							}
+					} else if (e.getItem().getTypeId() == 368) {
+						if (plugin.getConfig().getBoolean(
+								"block.Throw.EnderPearls")
+								&& !player
+										.hasPermission("entitymanager.enderpearls")) {
+							e.setCancelled(true);
+							messager(e);
+							alerter(e);
+						}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
+						}
+					} else if (e.getItem().getTypeId() == 381) {
+						if (plugin.getConfig().getBoolean(
+								"block.Throw.EnderEyes")
+								&& !player
+										.hasPermission("entitymanager.endereyes")) {
+							e.setCancelled(true);
+							messager(e);
+							alerter(e);
+						}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
+						}
+					} else if (e.getItem().getTypeId() == 333) {
+						if (plugin.getConfig().getBoolean(
+								"block.Entities.Boats")
+								&& !player.hasPermission("entitymanager.boats")) {
+							e.setCancelled(true);
+							messager(e);
+						}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to use a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
+						}
+					} else if (e.getItem().getTypeId() == 328) {
+						if (plugin.getConfig().getBoolean(
+								"block.Entities.Minecarts")
+								&& !player
+										.hasPermission("entitymanager.minecarts")) {
+							e.setCancelled(true);
+							messager(e);
+						}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to use a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
+						}
+					} else if (e.getItem().getTypeId() == 373) {
+						if (plugin.getConfig()
+								.getBoolean("block.Throw.Potions")
+								&& !player
+										.hasPermission("entitymanager.potions")) {
+							e.setCancelled(true);
+							messager(e);
+						}
+						if (plugin.getConfig().getBoolean(
+								"EntityManager.Logging")) {
+							log.log(Level.WARNING, "[EntityManager] "
+									+ e.getPlayer().getDisplayName()
+											.toLowerCase()
+									+ " tried to throw a "
+									+ e.getItem().getType().toString()
+											.toLowerCase());
+							return;
+						}
 					}
-					 if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-						 Location loc = e.getClickedBlock().getLocation();	
-					if (e.getItem().getTypeId() == 383) {
-							if (e.getItem().getDurability() == 200 && e.getPlayer().hasPermission("entitymanager.crystal")) {							
-								e.getClickedBlock().getWorld()
-										.spawn(loc, EnderCrystal.class);
-								return;
-							}
+					if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+						Location loc = e.getClickedBlock().getLocation();
+						if (e.getItem().getTypeId() == 383) {
+							if (e.getItem().getDurability() == 200) {
+								if (e.getPlayer().hasPermission(
+										"entitymanager.crystal")) {
+									e.getClickedBlock().getWorld()
+											.spawn(loc, EnderCrystal.class);
+									return;
+								} else {
+									e.getPlayer()
+											.sendMessage(
+													ChatColor.GREEN
+															+ "[EM]"
+															+ "You don't have permission to place ender crystals!");
+								}
+								if (plugin.getConfig().getBoolean(
+										"EntityManager.Logging")) {
+									log.log(Level.WARNING, "[EntityManager] "
+											+ e.getPlayer().getDisplayName()
+													.toLowerCase()
+											+ " tried to place a Ender Crystal");
+								}
 							}
 						}
+					}
 				}
 			}
 		}
 	}
-						public void alert(Player player, PlayerInteractEvent e) {
-								Bukkit.broadcast(ChatColor.GREEN + "[EM] " + ChatColor.DARK_RED
-										+ e.getPlayer().getDisplayName() + " Tryed to throw a "
-										+ ChatColor.GOLD + e.getItem().getType() + ".",
-										"entitymanager.admin");										
-								return;
-							}
-						}
+	public void alerter(PlayerInteractEvent e) {
+		final Player player = e.getPlayer();
+		if(plugin.getConfig().getBoolean("Send-Alerts")) {
+			for(Player p : player.getServer().getOnlinePlayers()) {
+				if(p.hasPermission("entitymanager.admin")) {
+					p.sendMessage(ChatColor.GREEN + "[EM] " + ChatColor.DARK_RED
+				+ e.getPlayer().getDisplayName() + " tryed to use a "
+				+ ChatColor.GOLD + e.getItem().getType().toString().toLowerCase() + ".");
+				}
+			}
+		}
+	}
+	public void messager(PlayerInteractEvent e) {
+		final Player player = e.getPlayer();
+		player.sendMessage(ChatColor.GREEN + "[EM] " + ChatColor.RED + "You dont have permission for " + e.getItem().getType().toString().toLowerCase() + "'s.");  
+	}
+}
