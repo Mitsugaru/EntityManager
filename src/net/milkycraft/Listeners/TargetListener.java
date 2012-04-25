f78c76303900a8ac29b0f730126d72096f5dbf8b
Nick Porillo <milkywayz@mail.com>
2012-04-25 17:04:01 -0400

    Slight changes

--- src/net/milkycraft/Listeners/TargetListener.java
+++ src/net/milkycraft/Listeners/TargetListener.java
@@ -10,28 +10,30 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent.TargetReason;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 
-public class TargetListener implements Listener{
+public class TargetListener implements Listener {
 	/*
 	 * This class is just about solid
 	 */
 	Spawnegg plugin;
+
 	public TargetListener(Spawnegg instance) {
 		plugin = instance;
-	}		
-	@EventHandler(priority = EventPriority.HIGH)
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onTarget(EntityTargetLivingEntityEvent e) {
 		Entity target = e.getTarget();
-	     if(target instanceof Player){
-	          Player player = (Player)target;
-			if(e.getReason() == TargetReason.CLOSEST_PLAYER 
-					|| e.getReason() ==  TargetReason.TARGET_ATTACKED_ENTITY) {
-				if(player.getItemInHand().getTypeId() == 383) {
-					if (player.hasPermission("entitymanager.avoid.target")){
+		if (target instanceof Player) {
+			Player player = (Player) target;
+			if (e.getReason() == TargetReason.CLOSEST_PLAYER
+					|| e.getReason() == TargetReason.TARGET_ATTACKED_ENTITY) {
+				if (player.getItemInHand().getTypeId() == 383) {
+					if (player.hasPermission("entitymanager.avoid.target")) {
 						e.setCancelled(true);
 						return;
-				}
+					}
 				}
 			}
-			}
-		}	
+		}
+	}
 }
