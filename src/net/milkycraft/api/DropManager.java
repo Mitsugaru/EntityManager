47ee1a0cd57f9418c62d204115ee74f263664351
Nick Porillo <milkywayz@mail.com>
2012-05-05 01:01:27 -0400

    Some misc changes

--- src/net/milkycraft/api/DropManager.java
+++ src/net/milkycraft/api/DropManager.java
@@ -4,6 +4,7 @@
 package net.milkycraft.api;
 
 import java.util.List;
+
 import net.milkycraft.ASEConfiguration.Settings;
 
 import org.bukkit.ChatColor;
@@ -19,49 +20,57 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
-
+// TODO: Auto-generated Javadoc
 /**
  * The Class DropManager.
  */
 public class DropManager implements Listener {
-	
+
 	/** The Constant instance. */
 	private final static DropManager instance = new DropManager();
 
 	/**
 	 * On attempted item drop.
-	 *
-	 * @param e the e
+	 * 
+	 * @param e
+	 *            the e
 	 */
-	@EventHandler(priority = EventPriority.HIGH)
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onAttemptedItemDrop(PlayerDropItemEvent e) {
 		final Player player = e.getPlayer();
-		final String mode = e.getPlayer().getGameMode().toString().toLowerCase();
+		final String mode = e.getPlayer().getGameMode().toString()
+				.toLowerCase();
 		final List<Integer> bitems = Settings.bitems;
 		final List<String> worldz = Settings.worlds;
-		for (String worldname : worldz) {
+		for (final String worldname : worldz) {
 			if (player.getWorld().getName().equals(worldname)) {
 				if (player.getGameMode() == GameMode.CREATIVE) {
-					if (!player.hasPermission("entitymanager.creative.drop") && Settings.cDrop) {
+					if (!player.hasPermission("entitymanager.creative.drop")
+							&& Settings.cDrop) {
 						e.setCancelled(true);
-						player.sendMessage(ChatColor.GREEN + "[EM] " + ChatColor.RED
-								+ "You dont have permission to drop items in " + mode + " mode.");	
-						return;
-					}
-				} 
-				if (player.getGameMode() == GameMode.SURVIVAL) {
-					if (!player.hasPermission("entitymanager.survival.drop") && Settings.sDrop) {
-						e.setCancelled(true);
-						player.sendMessage(ChatColor.GREEN + "[EM] " + ChatColor.RED
-								+ "You dont have permission to drop items in " + mode + " mode.");	
+						player.sendMessage(ChatColor.GREEN + "[EM] "
+								+ ChatColor.RED
+								+ "You dont have permission to drop items in "
+								+ mode + " mode.");
 						return;
 					}
 				}
-				for (Integer bitem : bitems) {
+				if (player.getGameMode() == GameMode.SURVIVAL) {
+					if (!player.hasPermission("entitymanager.survival.drop")
+							&& Settings.sDrop) {
+						e.setCancelled(true);
+						player.sendMessage(ChatColor.GREEN + "[EM] "
+								+ ChatColor.RED
+								+ "You dont have permission to drop items in "
+								+ mode + " mode.");
+						return;
+					}
+				}
+				for (final Integer bitem : bitems) {
 					if (e.getItemDrop().getItemStack().getTypeId() == bitem) {
 						if (!player
 								.hasPermission("entitymanager.bypass.blacklist")) {
-							Material mat = new ItemStack(bitem).getType();
+							final Material mat = new ItemStack(bitem).getType();
 							e.setCancelled(true);
 							player.sendMessage(ChatColor.GREEN + "[EM]"
 									+ ChatColor.RED + "The item: "
@@ -78,21 +87,28 @@ public class DropManager implements Listener {
 
 	/**
 	 * On entity death.
-	 *
-	 * @param e the e
+	 * 
+	 * @param e
+	 *            the e
 	 */
 	@EventHandler(priority = EventPriority.LOW)
 	public void onEntityDeath(EntityDeathEvent e) {
 		final Entity player = e.getEntity();
 		final List<Integer> bitems = Settings.bitems;
 		final List<String> worldz = Settings.worlds;
-		for (String worldname : worldz) {
+		for (final String worldname : worldz) {
 			if (e.getEntity().getWorld().getName().equals(worldname)) {
 				if (Settings.onDeath) {
 					e.getDrops().clear();
 					return;
 				}
-				for (Integer bitem : bitems) {
+				/*
+				 * Filters through the drops of dead entities If its a player,
+				 * check if they have permission, if they dont remove the
+				 * blacklisted drops If its any other entity, remove the blocked
+				 * items from there droppings
+				 */
+				for (final Integer bitem : bitems) {
 					if (e.getEntity() instanceof Player) {
 						if (!((Player) player)
 								.hasPermission("entitymanager.bypass.blacklist")) {
@@ -108,14 +124,15 @@ public class DropManager implements Listener {
 
 	/**
 	 * On block break.
-	 *
-	 * @param e the e
+	 * 
+	 * @param e
+	 *            the e
 	 */
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onBlockBreak(BlockBreakEvent e) {
 		final List<String> worldz = Settings.worlds;
 		final List<Integer> bitems = Settings.bitems;
-		for (String worldname : worldz) {
+		for (final String worldname : worldz) {
 			if (e.getBlock().getWorld().getName().equals(worldname)) {
 				if (Settings.bDestroy
 						&& !e.getPlayer().hasPermission(
@@ -123,7 +140,7 @@ public class DropManager implements Listener {
 					e.getBlock().getDrops().clear();
 					return;
 				}
-				for (Integer bitem : bitems) {
+				for (final Integer bitem : bitems) {
 					if (e.getBlock().getTypeId() == bitem) {
 						e.getBlock().getDrops().remove(bitem);
 					}
@@ -132,11 +149,11 @@ public class DropManager implements Listener {
 		}
 	}
 
-/**
- * Gets the drop manager.
- *
- * @return The dropmanager, so far nothing to get...
- */
+	/**
+	 * Gets the drop manager.
+	 * 
+	 * @return The dropmanager, so far nothing to get...
+	 */
 	public static DropManager getDropManager() {
 		return instance;
 	}
