0385d6e87a56d44b7c0fccbda0a8dc58f2aceb2f
Nick Porillo <milkywayz@mail.com>
2012-05-05 00:58:48 -0400

    Added some api methods that aren't tested

--- src/net/milkycraft/Listeners/ExpListener.java
+++ src/net/milkycraft/Listeners/ExpListener.java
@@ -16,20 +16,24 @@ import org.bukkit.event.entity.ExpBottleEvent;
 
 // TODO: Auto-generated Javadoc
 /**
- * The listener interface for receiving exp events.
- * The class that is interested in processing a exp
- * event implements this interface, and the object created
- * with that class is registered with a component using the
- * component's <code>addExpListener<code> method. When
+ * The listener interface for receiving exp events. The class that is interested
+ * in processing a exp event implements this interface, and the object created
+ * with that class is registered with a component using the component's
+ * <code>addExpListener<code> method. When
  * the exp event occurs, that object's appropriate
  * method is invoked.
  *
  * @see ExpEvent
  */
+
 public class ExpListener implements Listener {
-	/*
-	 * Class tested and working
-	 */
+	
+	/** The Constant clazz. */
+	private static final ExpListener clazz = new ExpListener();
+	
+	/** On xp drop. */
+	private int xp = 0;
+	
 	/**
 	 * On xp drop.
 	 *
@@ -39,10 +43,10 @@ public class ExpListener implements Listener {
 	public void onXpDrop(EntityDeathEvent e) {
 		final List<String> worldz = Settings.worlds;
 		final World world = e.getEntity().getWorld();
-		for (String worldname : worldz) {
-			if (world.getName().equals(worldname)) {
+		for (final String worldname : worldz) {
+			if (Settings.world || world.getName().equals(worldname)) {
 				if (Settings.totalexp) {
-					e.setDroppedExp(0);
+					e.setDroppedExp(xp);
 				}
 			}
 		}
@@ -50,20 +54,39 @@ public class ExpListener implements Listener {
 
 	/**
 	 * On exp explode.
-	 *
-	 * @param e the e
+	 * 
+	 * @param e
+	 *            the ExpBottleEvent
 	 */
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onExpExplode(ExpBottleEvent e) {
 		final List<String> worldz = Settings.worlds;
 		final World world = e.getEntity().getWorld();
-		for (String worldname : worldz) {
-			if (world.getName().equals(worldname)) {
+		for (final String worldname : worldz) {
+			if (Settings.world || world.getName().equals(worldname)) {
 				if (Settings.totalexp) {
 					e.setExperience(0);
 					e.setShowEffect(false);
 				}
 			}
 		}
+	}
+	
+	/**
+	 * Sets the exp dropped.
+	 *
+	 * @param exp the new exp dropped
+	 */
+	public void setExpDropped(int exp) {
+		xp = exp;
+	}
+	
+	/**
+	 * Gets the this.
+	 *
+	 * @return the this
+	 */
+	public static ExpListener getThis() {
+		return clazz;
 	}
 }
