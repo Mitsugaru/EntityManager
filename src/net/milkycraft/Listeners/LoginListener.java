93849ef505e3e58f0c8da46c9c557f5cf1b621ce
Nick Porillo <milkywayz@mail.com>
2012-05-05 00:59:10 -0400

    Added a api method and few misc changes

--- src/net/milkycraft/Listeners/LoginListener.java
+++ src/net/milkycraft/Listeners/LoginListener.java
@@ -1,8 +1,6 @@
-/*
- * 
- */
 package net.milkycraft.Listeners;
 
+import net.milkycraft.Spawnegg;
 import net.milkycraft.ASEConfiguration.Settings;
 
 import org.bukkit.ChatColor;
@@ -13,17 +11,33 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 // TODO: Auto-generated Javadoc
 /**
- * The listener interface for receiving login events.
- * The class that is interested in processing a login
- * event implements this interface, and the object created
- * with that class is registered with a component using the
+ * The listener interface for receiving login events. The class that is
+ * interested in processing a login event implements this interface, and the
+ * object created with that class is registered with a component using the
  * component's <code>addLoginListener<code> method. When
  * the login event occurs, that object's appropriate
  * method is invoked.
- *
+ * 
  * @see LoginEvent
  */
 public class LoginListener implements Listener {
+
+	/** The plugin. */
+	Spawnegg plugin;
+
+	/**
+	 * Instantiates a new clear sign.
+	 * 
+	 * @param instance
+	 *            the instance
+	 */
+	public LoginListener(Spawnegg instance) {
+		plugin = instance;
+	}
+
+	/** The motd. */
+	private String motd = ChatColor.GREEN
+			+ "This server is running EntityManager 3.5 by milkywayz";
 
 	/**
 	 * On login.
@@ -33,11 +47,18 @@ public class LoginListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onLogin(PlayerJoinEvent e) {
 		if (Settings.Motd) {
-			e.getPlayer()
-					.sendMessage(
-							ChatColor.GREEN
-									+ "This server is running EntityManager by milkywayz");
+			e.getPlayer().sendMessage(motd);
 			return;
 		}
 	}
+
+	/**
+	 * Gets the motd.
+	 *
+	 * @return the motd
+	 */
+	public String getMotd() {
+		return motd;
+	}
+
 }
