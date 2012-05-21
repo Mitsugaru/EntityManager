a1fa96f2c311b221f25b0667d0245063f6e817e8
Nick Porillo <milkywayz@mail.com>
2012-05-21 18:28:51 -0400

    Rewritten for performance in mind

--- src/net/milkycraft/Listeners/EnchantListener.java
+++ src/net/milkycraft/Listeners/EnchantListener.java
@@ -15,7 +15,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 
-// TODO: Auto-generated Javadoc
 /**
  * The listener interface for receiving enchant events. The class that is
  * interested in processing a enchant event implements this interface, and the
@@ -28,9 +27,6 @@ import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
  */
 public class EnchantListener implements Listener {
 
-	/** The perm. */
-	private final String perm = ChatColor.GREEN + "[EM]" + ChatColor.RED
-			+ " You dont have permission to enchant items!";
 
 	/**
 	 * On enchant attempt.
@@ -42,28 +38,42 @@ public class EnchantListener implements Listener {
 	public void onEnchantAttempt(PrepareItemEnchantEvent e) {
 		final List<String> worldz = Settings.worlds;
 		final World world = e.getEnchantBlock().getWorld();
-		for (final String worldname : worldz) {
-			if (world.getName() == worldname || Settings.world) {
+		if(Settings.world) {
+			if(Settings.totalenchant && !e.getEnchanter().hasPermission("entitymanager.enchanting")) {
+				e.setCancelled(true);
+				e.getEnchanter().sendMessage(ChatColor.GREEN + "[EM]" + ChatColor.RED
+						+ " You dont have permission to enchant items!");
+				alert(e);
+				return;
+			}
+		} else {
+		for (String worldname : worldz) {
+			if (world.getName() == worldname) {
 				if (Settings.totalenchant
 						&& !e.getEnchanter().hasPermission(
 								"entitymanager.enchanting")) {
 					e.setCancelled(true);
-					e.getEnchanter().sendMessage(perm);
-					if (Settings.alertz) {
-						for (final Player p : e.getEnchanter().getServer()
-								.getOnlinePlayers()) {
-							if (p.hasPermission("entitymanager.admin")) {
-								p.sendMessage(ChatColor.GREEN
-										+ "[EM] "
-										+ ChatColor.DARK_RED
-										+ e.getEnchanter().getDisplayName()
-										+ " tried to enchant a "
-										+ ChatColor.GOLD
-										+ e.getItem().getType().toString()
-												.toLowerCase() + ".");
-							}
-						}
-					}
+					e.getEnchanter().sendMessage(ChatColor.GREEN + "[EM]" + ChatColor.RED
+							+ " You dont have permission to enchant items!");
+					alert(e);
+				}
+				}
+			}
+		}
+	}
+	public void alert(PrepareItemEnchantEvent e) {
+		if (Settings.alertz) {
+			for (Player p : e.getEnchanter().getServer()
+					.getOnlinePlayers()) {
+				if (p.hasPermission("entitymanager.admin")) {
+					p.sendMessage(ChatColor.GREEN
+							+ "[EM] "
+							+ ChatColor.DARK_RED
+							+ e.getEnchanter().getDisplayName()
+							+ " tried to enchant a "
+							+ ChatColor.GOLD
+							+ e.getItem().getType().toString()
+									.toLowerCase() + ".");
 				}
 			}
 		}
