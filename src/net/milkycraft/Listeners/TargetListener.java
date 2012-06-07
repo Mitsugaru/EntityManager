459a63971b6a8e2e248fa57e4f6877546a02e70e
Nick Porillo <milkywayz@mail.com>
2012-06-06 20:14:45 -0400

    Extended entitymanager and added javadoc

--- src/net/milkycraft/Listeners/TargetListener.java
+++ src/net/milkycraft/Listeners/TargetListener.java
@@ -1,13 +1,32 @@
 package net.milkycraft.listeners;
 
+import net.milkycraft.EntityManager;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 
-public class TargetListener implements Listener {
+// TODO: Auto-generated Javadoc
+/**
+ * The listener interface for receiving target events.
+ * The class that is interested in processing a target
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addTargetListener<code> method. When
+ * the target event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see TargetEvent
+ */
+public class TargetListener extends EntityManager implements Listener {
 
+	/**
+	 * On target.
+	 *
+	 * @param e the e
+	 */
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onTarget(EntityTargetLivingEntityEvent e) {
 		if (e.getTarget() instanceof Player) {
