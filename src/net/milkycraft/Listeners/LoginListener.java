d205f7ea0b8a9151a21d837e8dbb236e87e0d156
Nick Porillo <milkywayz@mail.com>
2012-06-06 20:13:42 -0400

    Added a api method to change the motd ingame (deprecated method)

--- src/net/milkycraft/Listeners/LoginListener.java
+++ src/net/milkycraft/Listeners/LoginListener.java
@@ -1,6 +1,5 @@
 package net.milkycraft.listeners;
 
-
 import net.milkycraft.EntityManager;
 import net.milkycraft.configuration.Settings;
 
@@ -10,48 +9,57 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-	public class LoginListener implements Listener {
+// TODO: Auto-generated Javadoc
+/**
+ * The listener interface for receiving login events.
+ * The class that is interested in processing a login
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addLoginListener<code> method. When
+ * the login event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see LoginEvent
+ */
+public class LoginListener extends EntityManager implements Listener {
 
-		/** The plugin. */
-		EntityManager plugin;
+	/** The motd. */
+	private String motd = ChatColor.GREEN
+			+ "This server is running EntityManager 3.7 by milkywayz";
 
-		/**
-		 * Instantiates a new clear sign.
-		 * 
-		 * @param instance
-		 *            the instance
-		 */
-		public LoginListener(EntityManager instance) {
-			plugin = instance;
+	/**
+	 * On login.
+	 * 
+	 * @param e
+	 *            the e
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onLogin(PlayerJoinEvent e) {
+		if (Settings.Motd) {
+			e.getPlayer().sendMessage(motd);
+			return;
 		}
+	}
 
-		/** The motd. */
-		private final static String motd = ChatColor.GREEN
-				+ "This server is running EntityManager 3.6.2 by milkywayz";
-
-		/**
-		 * On login.
-		 * 
-		 * @param e
-		 *            the e
-		 */
-		@EventHandler(priority = EventPriority.MONITOR)
-		public void onLogin(PlayerJoinEvent e) {
-			if (Settings.Motd) {
-				e.getPlayer().sendMessage(motd);
-				return;
-			}
-		}
-
-		/**
-		 * Gets the motd.
-		 * 
-		 * @return the motd
-		 */
-		public static String getMotd() {		
-			return motd;
-			
-		}
+	/**
+	 * Gets the motd.
+	 * 
+	 * @return the message of the day for entitymanager
+	 */
+	public final String getMotd() {
+		return motd;
 
 	}
 
+	/**
+	 * Set the motd.
+	 *
+	 * @param message the new motd
+	 * @deprecated MOTD will reset upon reload or restart therefore call this on in your plugins onEnable()
+	 */
+	public void setMotd(String message) {
+		writeWarn("MOTD was set to: " + message);
+		motd = message;
+	}
+
+}
